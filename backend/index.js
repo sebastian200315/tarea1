9f632a0eff9d4dcf0c1d8a3b52ddb07a0edad83c
Jimy C <beatsebastian02@gmail.com>
2025-05-20 09:49:13 -0500

    Primera ve

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const app = express(); // Creamos la app
+const express = require('express')
+const app = express()
 
-app.use(cors()); // ✅ Aplicamos CORS a la app
-
-const port = 3000;
+const port = 3000
 
 app.get('/prueba', (req, res) => {
-  res.send('¡Hola Mundo!');
-});
+  res.send('Hola Mundo!')
+})
 
 app.listen(port, () => {
   console.log(`Servidor ejecutándose en http://localhost:${port}`);
