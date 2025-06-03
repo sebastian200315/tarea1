5ea11dea1212fb8f6273c679ddbe42279ad76dc8
Jimy C <beatsebastian02@gmail.com>
2025-06-02 20:25:47 -0500

    libros corregido

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,15 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-app.use(cors());  // Usa la instancia 'app' para middleware
+app.use(cors());  // Habilita CORS para todas las rutas
 
-<<<<<<< HEAD
-const port = 3000
-=======
-const port = 8080
->>>>>>> 1da4b13020f7c303b271bfef075666bd3aeecc3c
+const port = 8080; // Puerto configurado a 8080 como solicitaste
 
 app.get('/prueba', (req, res) => {
-  res.send('Hola Mundo!')
-})
+  res.send('Hola Mundo!');
+});
 
 app.listen(port, () => {
-  console.log(`Servidor ejecutándose en http://localhost:${port}`)
-})
-<<<<<<< HEAD
-=======
-
->>>>>>> 1da4b13020f7c303b271bfef075666bd3aeecc3c
+  console.log(`Servidor ejecutándose en http://localhost:${port}`);
+});
